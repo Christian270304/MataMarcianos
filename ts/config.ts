4f5af655cd3608f007614f8e81aae56178905b0a
danielagmz <danielagmz@users.noreply.github.com>
2025-01-13 01:28:28 +0100

    chore: agrega configuracion, interfaces y jquery

--- ts/config.ts
+++ ts/config.ts
@@ -3,5 +3,6 @@ const COLUMNES = 8;		// Nombre de columnes de l'exèrcit dels aliens
 const ALIENS = FILES * COLUMNES;	// Nombre total d'
 const WIDTH = 640;	// Amplada de l'àrea de joc
 const HEIGHT = 480;	// Alçada de l'àrea de joc
+const BALASCALE = 0.1;
 
-export { FILES, COLUMNES, ALIENS, WIDTH, HEIGHT };
+export { FILES, COLUMNES, ALIENS, WIDTH, HEIGHT, BALASCALE };
---
+++ ts/interfaces.ts
@@ -0,0 +1,6 @@
+enum directions {
+    LEFT,
+    RIGHT
+}
+
+export { directions };
